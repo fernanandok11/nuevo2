d09fc792908a63921d33516d7b6493309f22556f
fernanandok11 <fernanandok11@gmail.com>
2022-09-24 09:44:30 -0300

    Modifique el main std, y crea al archiv  funciones

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
-    std::cout << "hola mundo" << std::endl;
+    cout << "hola mundo" << endl;
+    
+    char pero[] = "Esta pruba va servir no ";
+
+    cout << pero;
     return 0;
+
 }
