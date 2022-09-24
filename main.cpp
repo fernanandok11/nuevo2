2381c864665ed173868db5c3a5b69b10ac59eb1c
FerUbutu <fernanandok11@gmail.com>
2022-09-24 11:47:24 -0300

    una cambio en main

--- main.cpp
+++ main.cpp
@@ -7,11 +7,14 @@ int main(int argc, char *argv[])
     cout << "hola mundo" << endl;
     
     char pero[] = "Esta pruba va servir no ";
-    char casa[] = " agrego un cadena desde git crhome"
+    char casa[] = " agrego un cadena desde git crhome";
         
     cout << pero;
     cout << casa;
-    
+
+    if(casa != NULL)
+        return -1;
+
     return 0;
 
 }
