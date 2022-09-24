d990a09fe53775a39b5b05f394c861888768a76a
fernanandok11 <37600005+fernanandok11@users.noreply.github.com>
2022-09-24 09:48:19 -0300

    Update main.cpp
    Agrego una cadena desde e crhome

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ int main(int argc, char *argv[])
     cout << "hola mundo" << endl;
     
     char pero[] = "Esta pruba va servir no ";
-
+    char casa[] = " agrego un cadena desde git crhome"
+        
     cout << pero;
+    cout << casa;
+    
     return 0;
 
 }
